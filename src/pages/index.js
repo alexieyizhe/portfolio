e5fecb1629b896b1632dfb9827b922f0f96c3e8c
Alex Xie <alexieyizhe@gmail.com>
2018-05-27 15:34:24 -0400

    Implement working command input

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,54 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import CommandInput from '../components/command_input.js'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {curCommand: ''};
+  }
 
-export default IndexPage
+  handleCommand(keyCode) {
+    switch(keyCode) {
+      case 'Enter': // match enter to issue command
+        this.setState({curCommand: ''});
+        break;
+
+      case 'Backspace': // match enter to issue command
+      this.setState((prevState) => {
+        return {curCommand: prevState.curCommand.substring(0, prevState.curCommand.length - 1)};
+      })
+      break;
+
+      case (keyCode.match(/[ -~]/) || {}).input: // match all printable non-return characters
+        this.setState((prevState) => {
+          return {curCommand: prevState.curCommand + (keyCode.length === 1 ? keyCode : '')};
+        })
+        break;
+    }
+
+    // for testing
+    console.log(keyCode);
+    console.log(this.state);
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Alex Xie</h1>
+        <p>Personal website/portfolio</p>
+        <p>
+          > <CommandInput
+              curCommand={this.state.curCommand}
+              editCommand={(k) => this.handleCommand(k)}
+            />
+        </p>
+      </div>
+    )
+  }
+}
+
+export default IndexPage;
