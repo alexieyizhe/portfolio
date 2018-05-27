ac24fbc1f9e946c50a5152eba5ffb24c9c26a761
Alex Xie <alexieyizhe@gmail.com>
2018-05-27 17:58:26 -0400

    Working shell of terminal design

--- README.md
+++ README.md
@@ -1,26 +1,10 @@
-# gatsby-starter-default
-The default Gatsby starter.
+# gatsby-starter-hello-world
+Starter with the bare essentials needed for a [Gatsby](https://www.gatsbyjs.org/) site
 
-For an overview of the project structure please refer to the [Gatsby documentation - Building with Components](https://www.gatsbyjs.org/docs/building-with-components/).
-
-## Install
-
-Make sure that you have the Gatsby CLI program installed:
-```sh
-npm install --global gatsby-cli
+Install this starter (assuming Gatsby is installed) by running from your CLI:
+```
+gatsby new gatsby-site https://github.com/gatsbyjs/gatsby-starter-hello-world
 ```
 
-And run from your CLI:
-```sh
-gatsby new gatsby-example-site
-```
-
-Then you can run it by:
-```sh
-cd gatsby-example-site
-npm run develop
-```
-
-## Deploy
-
-[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-default)
+## Running in development
+`gatsby develop`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,13 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import CommandInput from '../components/command_input.js'
+import React from 'react';
+import Link from 'gatsby-link';
+import FontAwesome from 'react-fontawesome';
+import TerminalWindow from '../components/terminalWindow.js';
+import '../styles/global.css';
 
-class IndexPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {curCommand: ''};
-  }
-
-  handleCommand(keyCode) {
-    switch(keyCode) {
-      case 'Enter': // match enter to issue command
-        this.setState({curCommand: ''});
-        break;
-
-      case 'Backspace': // match enter to issue command
-      this.setState((prevState) => {
-        return {curCommand: prevState.curCommand.substring(0, prevState.curCommand.length - 1)};
-      })
-      break;
-
-      case (keyCode.match(/[ -~]/) || {}).input: // match all printable non-return characters
-        this.setState((prevState) => {
-          return {curCommand: prevState.curCommand + (keyCode.length === 1 ? keyCode : '')};
-        })
-        break;
-    }
-
-    // for testing
-    console.log(keyCode);
-    console.log(this.state);
-  }
-
-  componentDidMount() {
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Alex Xie</h1>
-        <p>Personal website/portfolio</p>
-        <p>
-          > <CommandInput
-              curCommand={this.state.curCommand}
-              editCommand={(k) => this.handleCommand(k)}
-            />
-        </p>
-      </div>
-    )
-  }
+function IndexPage(props) {
+  return (
+    <TerminalWindow />
+  )
 }
 
 export default IndexPage;
