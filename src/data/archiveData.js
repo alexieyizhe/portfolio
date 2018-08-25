d9297377f72847aeb53f03b247b5d309984d7d34
Alex Xie <alexieyizhe@gmail.com>
2018-08-24 22:58:50 -0400

    Add link to hackenger hunt to archive

--- src/data/archiveData.js
+++ src/data/archiveData.js
@@ -5,6 +5,11 @@ export const archiveSiteList = [
     desc: 'A snapshot of alexieyizhe.me in May 2018. This iteration was the final one before v2 (the current site you\'re seeing here).',
   },
   {
+    name: 'Equithon Hackenger Hunt',
+    url: '/archive/equithon-hackenger-hunt/start.html',
+    desc: 'A playable version of the hackenger hunt that hackers at Equithon 2018 attempted.',
+  },
+  {
     name: 'Jan 2018',
     url: '/archive/jan-2018/',
     desc: 'A snapshot of alexieyizhe.me in Jan 2018. This was the initial iteration of my portfolio site without much polish.',
