9406af3f9790f75c744588491490072aec3131f9
Alex Xie <alexieyizhe@gmail.com>
2019-08-28 06:34:40 -0400

    Add ability to use absolute imports

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,23 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-typescript`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          "~src": "src",
+          "~assets": "src/static",
+          "~components": "src/components",
+          "~layouts": "src/layouts",
+          "~pages": "src/pages",
+          "~sections": "src/sections",
+          "~utils": "src/utils",
+        },
+        extensions: ["ts", "tsx", "json"],
+      },
+    },
+  ],
 };
