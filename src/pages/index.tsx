4eae86ad06a2187a99f436d95bc7783cc00757ac
Alex Xie <alexieyizhe@gmail.com>
2019-08-28 05:18:24 -0400

    :gear: Setup base project

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IndexPage = () => (
+  <div>
+    <h1>
+      hello dis the active dev build of me new site! check back later for more
+      details
+    </h1>
+  </div>
+);
+
+export default IndexPage;
