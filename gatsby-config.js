9e767d30c39b9e2824f94f5908b1bd10643bd5bd
Alex Xie <alexieyizhe@gmail.com>
2019-08-28 06:54:29 -0400

    :wrench: Add Github Actions CI and absolute imports (#30)
    * Start workflow
    
    * Try
    
    * Add runs-on
    
    * Add more steps
    
    * Finish CI
    
    * Made an oopsie
    
    * Run all in same job
    
    * push broken code to make sure workflow fails
    
    * Fix
    
    * Add ability to use absolute imports
    
    * Clean up descriptions and remove gatsby starter references

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `alexxie.ca`,
+    description: ``,
+    author: `@alexieyizhe`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-typescript`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          "~src": "src",
+          "~assets": "src/static",
+          "~components": "src/components",
+          "~layouts": "src/layouts",
+          "~pages": "src/pages",
+          "~sections": "src/sections",
+          "~utils": "src/utils",
+        },
+        extensions: ["ts", "tsx", "json"],
+      },
+    },
+  ],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import NotFoundPage from "~pages/404";
+
 const IndexPage = () => (
   <div>
     <h1>
       hello dis the active dev build of me new site! check back later for more
       details
     </h1>
+    <NotFoundPage />
   </div>
 );
 
