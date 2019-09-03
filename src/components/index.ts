ebb831442c2e80d336c09b0433d2e79093c1ad25
Alex Xie <alexieyizhe@gmail.com>
2019-09-02 21:05:13 -0400

    Export everything

---
+++ src/components/index.ts
@@ -0,0 +1,26 @@
+export { default as Button } from "./Button";
+export * from "./Button";
+
+export { default as Card } from "./Card";
+export * from "./Card";
+
+export { default as ContentCard } from "./ContentCard";
+export * from "./ContentCard";
+
+export { default as Icon } from "./Icon";
+export * from "./Icon";
+
+export { default as Link } from "./Link";
+export * from "./Link";
+
+export { default as PageWrapper } from "./PageWrapper";
+export * from "./PageWrapper";
+
+export { default as Particle } from "./Particle";
+export * from "./Particle";
+
+export { default as ShowcaseCard } from "./ShowcaseCard";
+export * from "./ShowcaseCard";
+
+export { default as Text } from "./Text";
+export * from "./Text";
