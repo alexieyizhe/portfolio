696aa0c894fdaf582b9b67e9a36bdc3b16154e4d
Alex Xie <alexieyizhe@gmail.com>
2019-09-09 17:53:11 -0400

    Bare minimum 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 import { SpookyScaryImg } from "~assets/images";
 
+import { Text } from "~src/components";
+
 const ScaredImg = styled.img`
   max-width: 250px;
 
@@ -10,8 +12,20 @@ const ScaredImg = styled.img`
   bottom: -40px;
   right: 20%;
 `;
+
+const HeadingContainer = styled.div`
+  margin: 25vh 20vw;
+`;
+
 const NotFoundPage = () => (
   <div>
+    <HeadingContainer>
+      <Text variant="heading">Oh no!</Text>
+      <Text variant="heading">
+        There&apos;s no page at {window.location.pathname}
+      </Text>
+    </HeadingContainer>
+
     <ScaredImg src={SpookyScaryImg} />
   </div>
 );
