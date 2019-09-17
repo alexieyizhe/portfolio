380dba205d0550e02c73c738e9c97d0889415483
Alex Xie <alexieyizhe@gmail.com>
2019-09-17 15:21:58 -0400

    Fix favicon

--- src/assets/favicons/index.ts
+++ src/assets/favicons/index.ts
@@ -1,12 +1,14 @@
 export { default as Favicon16Triangle } from "./favicon-16x16-triangle.png";
 export { default as Favicon32Triangle } from "./favicon-32x32-triangle.png";
-
 export { default as FaviconICOTriangle } from "./favicon-triangle.ico";
 
 export { default as Favicon16Circle } from "./favicon-16x16-circle.png";
 export { default as Favicon32Circle } from "./favicon-32x32-circle.png";
-
 export { default as FaviconICOCircle } from "./favicon-circle.ico";
+
+export { default as Favicon16Square } from "./favicon-16x16-square.png";
+export { default as Favicon32Square } from "./favicon-32x32-square.png";
+export { default as FaviconICOSquare } from "./favicon-square.ico";
 
 export { default as AppleTouchIcon } from "./apple-touch-icon.png";
 export { default as SafariPinnedTabIcon } from "./safari-pinned-tab.svg";
--- src/components/Helmet/index.tsx
+++ src/components/Helmet/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Helmet as ReactHelmet } from "react-helmet";
 
 import copy from "~assets/copy";
@@ -10,6 +10,9 @@ import {
   Favicon16Circle,
   Favicon32Circle,
   FaviconICOCircle,
+  Favicon16Square,
+  Favicon32Square,
+  FaviconICOSquare,
   AppleTouchIcon,
   SafariPinnedTabIcon,
 } from "~assets/favicons";
@@ -23,7 +26,32 @@ const Helmet: React.FC<HelmetProps> = ({
   title,
   description = copy.seo.description,
 }) => {
-  const renderedTitle = `Alex Xie${title ? ` | ${title}` : ""}`;
+  const renderedTitle = useMemo(() => `Alex Xie${title ? ` | ${title}` : ""}`, [
+    title,
+  ]);
+  const renderedIcons = useMemo(() => {
+    const prob = Math.random();
+
+    if (prob < 0.3) {
+      return {
+        size16x16: Favicon16Triangle,
+        size32x32: Favicon32Triangle,
+        ico: FaviconICOTriangle,
+      };
+    } else if (prob < 0.7) {
+      return {
+        size16x16: Favicon16Circle,
+        size32x32: Favicon32Circle,
+        ico: FaviconICOCircle,
+      };
+    } else {
+      return {
+        size16x16: Favicon16Square,
+        size32x32: Favicon32Square,
+        ico: FaviconICOSquare,
+      };
+    }
+  }, []);
 
   return (
     <ReactHelmet>
@@ -38,18 +66,16 @@ const Helmet: React.FC<HelmetProps> = ({
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href={Math.random() < 0.5 ? Favicon32Triangle : Favicon32Circle}
+        href={renderedIcons.size32x32}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href={Math.random() < 0.5 ? Favicon16Triangle : Favicon16Circle}
+        href={renderedIcons.size16x16}
       />
-      <link
-        rel="shortcut icon"
-        href={Math.random() < 0.5 ? FaviconICOTriangle : FaviconICOCircle}
-      />
+      <link rel="shortcut icon" href={renderedIcons.ico} />
+
       <link rel="mask-icon" href={SafariPinnedTabIcon} color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#00aba9" />
 
