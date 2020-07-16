589db559d76a245539e41e62cd46b2042fb703fe
Alex Xie <alexieyizhe@gmail.com>
2020-07-16 16:27:16 -0400

    Lazy load sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import loadable from "@loadable/component";
 
 import PageWrapper from "~components/PageWrapper";
 import LandingSection from "~sections/landing";
 
-const importFeatured = () =>
-  import("../sections/featured").then(component => component.default);
-const importAbout = () =>
-  import("../sections/about").then(component => component.default);
-const importShowcase = () =>
-  import("../sections/showcase").then(component => component.default);
-const importFooter = () =>
-  import("../components/Footer").then(component => component.default);
+const Featured = loadable(() => import("~sections/featured"));
+const About = loadable(() => import("~sections/about"));
+const Showcase = loadable(() => import("~sections/showcase"));
+const Footer = loadable(() => import("~components/Footer"));
 
-const IndexPage = () => {
-  const [FeaturedSection, setFeaturedSection] = useState<React.FC | null>(null);
-  const [AboutSection, setAboutSection] = useState<React.FC | null>(null);
-  const [ShowcaseSection, setShowcaseSection] = useState<React.FC | null>(null);
-  const [FooterSection, setFooterSection] = useState<React.FC | null>(null);
-
-  useEffect(() => {
-    const loadComponents = async () => {
-      const featuredSection = await importFeatured();
-      setFeaturedSection(featuredSection);
-
-      const aboutSection = await importAbout();
-      setAboutSection(aboutSection);
-
-      const showcaseSection = await importShowcase();
-      setShowcaseSection(showcaseSection);
-
-      const footerSection = await importFooter();
-      setFooterSection(footerSection);
-    };
-
-    loadComponents();
-  }, []);
-
-  return (
-    <PageWrapper>
-      <LandingSection />
-      {FeaturedSection}
-      {AboutSection}
-      {ShowcaseSection}
-      {FooterSection}
-    </PageWrapper>
-  );
-};
-
+const IndexPage = () => (
+  <PageWrapper>
+    <LandingSection />
+    <Featured />
+    <About />
+    <Showcase />
+    <Footer />
+  </PageWrapper>
+);
 export default IndexPage;
