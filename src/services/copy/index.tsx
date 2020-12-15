c254f927b78cb6214a213a5cfd47fb9937342f42
Alex Xie <alexieyizhe@gmail.com>
2020-12-14 20:26:47 -0500

    Now playing working

--- src/services/copy/index.tsx
+++ src/services/copy/index.tsx
@@ -1,5 +1,5 @@
 import { h, createContext, FunctionalComponent } from 'preact';
-import { useContext } from 'preact/hooks';
+import { useContext, useEffect, useState } from 'preact/hooks';
 import { getDateInZone, getRandomItem } from 'services/utils';
 import {
   ACTIVITIES,
@@ -9,11 +9,14 @@ import {
   TALKING_POINTS,
 } from './config';
 
-type TNowPlayingInfo = {
-  type: 'now-playing';
+type TNowPlayingData = {
   name: string;
   by: string; // artist or author or show or whatnot
 };
+
+type TNowPlayingInfo = {
+  type: 'now-playing';
+} & TNowPlayingData;
 
 type TActivityInfo = {
   type: 'activity';
@@ -26,6 +29,42 @@ export type TCopyContextValue = {
   currentDate: Date;
   additionalInfo: TNowPlayingInfo | TActivityInfo;
   talkingPoint: string; // wanna chat about ...
+};
+
+// returns null if nothing is playing
+const getNowPlaying = async (): Promise<TNowPlayingData | null | undefined> => {
+  try {
+    const res = await fetch(
+      'https://api.spotify.com/v1/me/player/currently-playing?additional_types=track,episode',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer BQAi3hZsf1Ofo7O9H3mDNBsnLpWn7W6eubO0NxgquXyWnn6Ee9yzBL661BtcG5_pY4f8l-J0PepJc1OmXkYFU9Y3yhUFuHsoQtxP3dJgj3kdPkicd7wLF9XvsYfqHVI3-se6dzvODgP46v7crn0GiUGIZhm4y10bOL4BvZKacA`,
+        },
+      }
+    );
+    console.log(res);
+
+    const isPlaying = res.status === 200; // API returns 204 if nothing is playing
+
+    if (!isPlaying) return null;
+
+    console.log(await res.json());
+
+    const {
+      item: {
+        name,
+        artists: [{ name: artistName }],
+      },
+    } = await res.json();
+
+    return {
+      name,
+      by: artistName,
+    };
+  } catch {
+    return undefined;
+  }
 };
 
 const currentEDTDate = getDateInZone('America/Toronto');
@@ -45,12 +84,26 @@ const useCopyContext = () => useContext(CopyContext);
 
 // todo: actually do stuff here
 const CopyContextProvider: FunctionalComponent = ({ children }) => {
+  const [nowPlayingData, setNowPlayingData] = useState<
+    TNowPlayingData | undefined | null
+  >(undefined);
+
   const greeting = getRandomItem(GREETINGS);
-  const additionalInfo = {
-    type: 'activity' as const,
-    content: ACTIVITIES[0],
-  };
   const talkingPoint = getRandomItem(TALKING_POINTS);
+
+  const additionalInfo =
+    nowPlayingData === undefined
+      ? { type: 'activity' as const, content: ACTIVITIES[0] }
+      : nowPlayingData === null
+      ? { type: 'activity' as const, content: ACTIVITIES[0] }
+      : { type: 'now-playing' as const, ...nowPlayingData };
+
+  useEffect(() => {
+    (async function fetchNowPlaying() {
+      const nowPlaying = await getNowPlaying();
+      setNowPlayingData(nowPlaying);
+    })();
+  }, []);
 
   return (
     <CopyContext.Provider
