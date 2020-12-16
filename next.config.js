fe1d8d852b799a72baf26cea1e5b3abd75d26343
Alex Xie <alexieyizhe@gmail.com>
2020-12-16 15:38:01 -0500

    feat: add bundle size analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 const withPreact = require('next-plugin-preact');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = withPreact({
+  webpack: (config, { isServer }) => {
+    if (process.env.ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: isServer ? 8888 : 8889,
+          openAnalyzer: true,
+        })
+      );
+    }
+    return config;
+  },
   experimental: {
     modern: true,
   },
