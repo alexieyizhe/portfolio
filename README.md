edafe0bc5ce7f84777b6746406b46ce1270bd641
Alex Xie <alexieyizhe@gmail.com>
2020-12-15 21:50:29 -0500

    Minor tweaks to font and scripts

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # hello friend this is my website
 
-`npm start` to do dev stuff
-
+`npm run dev` to do dev stuff
 `npm run build` to do production stuff
+
+still to do
+
+- [ ] visual regression testing
+- [ ] theme & dark mode?
--- src/components/core/Text.tsx
+++ src/components/core/Text.tsx
@@ -2,5 +2,6 @@ import { styled } from 'goober';
 
 export const Text = styled('span')`
   font-size: 16px;
-  font-family: 'Space Grotesk', sans-serif;
+  font-family: 'Space Grotesk', -apple-system, BlinkMacSystemFont, Roboto,
+    Ubuntu, 'Helvetica Neue', sans-serif sans-serif;
 `;
