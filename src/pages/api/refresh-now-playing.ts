135440d5fbf153088b9f2979cbda8177aa959c24
Alex Xie <alexieyizhe@gmail.com>
2020-12-19 13:05:24 -0500

    Error handling for refresh-now-playing

--- src/pages/api/refresh-now-playing.ts
+++ src/pages/api/refresh-now-playing.ts
@@ -3,11 +3,16 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { StorageClient } from 'services/_server_';
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
-  const client = new StorageClient();
-  const { token } = await client.getSpotifyCredentials();
-  client.disconnect();
+  try {
+    const client = new StorageClient();
+    const { token } = await client.getSpotifyCredentials();
+    client.disconnect();
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ success: !!token }));
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ success: !!token }));
+  } catch (e) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ reason: JSON.stringify(e) }));
+  }
 }
