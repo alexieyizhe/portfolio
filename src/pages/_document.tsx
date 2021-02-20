526f810a9a79a0c55b0405d4a8a07cbd9ff88ada
Alex Xie <alexieyizhe@gmail.com>
2021-02-20 16:55:41 -0500

    Add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,6 +30,8 @@ export default class CustomDocument extends Document<{ css: string }> {
         <body>
           <Main />
           <NextScript />
+          <script data-skip-dnt="true" data-hostname="alexxie.com" async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+          <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt=""/></noscript>
         </body>
       </Html>
     );
